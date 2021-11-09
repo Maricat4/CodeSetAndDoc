ad11ef3d46e5c30e8b0a5a7bae87b38aa12c1b77
Maricat4 <1411288042@qq.com>
2021-11-09 19:03:56 +0800

    task 0

--- CodeSet/Code1/Program.cs
+++ CodeSet/Code1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!111");
+//System.Console.WriteLine("Hello, World!66");
+using System;
+namespace myApp{
+
+    class Name
+    {
+        public static void Main(string[] args){
+            System.Console.WriteLine("Hello, World!666");
+            var a = Console.ReadLine();
+
+            Console.WriteLine(a);
+            var t1 = Console.ReadLine();
+            Console.WriteLine(t1);
+            Console.ReadLine();
+        }
+    }
+}
