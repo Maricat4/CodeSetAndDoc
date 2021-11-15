a321a02822a282a71639ace3343e7af72d734fda
Maricat4 <1411288042@qq.com>
2021-11-16 00:05:18 +0800

    task 01

--- CodeSet/Code1/ClassCode.cs
+++ CodeSet/Code1/ClassCode.cs
@@ -12,12 +12,23 @@ namespace ClassCode
             // Console.WriteLine(tmp.Output(1,2,4,5,1));
             // Console.WriteLine(tmp.Output(1,2,4,5,"gggggg"));
             // Console.WriteLine(tmp.a);
+            // Console.WriteLine(ClassA._stB);
+            ClassA tmp = new ClassA(4);
+            Console.WriteLine(ClassA._constValue);
             Console.WriteLine(ClassA._stB);
+            Console.WriteLine(tmp.b);
+            Person tmpName = new Person("汪","雨航");
+            Console.WriteLine(tmpName.FullName);   
+
         }
     }
 
     class ClassA
     {
+
+        //const成员
+        public const int _constValue = 5;
+        public readonly int _readonlyValue;
         //构造函数
         static ClassA(){
             Console.WriteLine("ClassA静态构造函数");
@@ -30,10 +41,12 @@ namespace ClassCode
         }
         //字段
         private int _a;
+        private int _b;
 
         public static int _stB;
         //属性
         public int a{get{return _a;}private set{}}
+        public int b{get{return _b;}private set{}}
     
         //可变参数
         public int Output(int a,int b = 1,int c = 2,int d = 4){
@@ -63,4 +76,15 @@ namespace ClassCode
             return sum;
         }
     }
+
+    //表达式属性
+    public class Person { 
+    public Person(string firstName, string lastName) { 
+        FirstName = firstName; 
+        LastName = lastName; 
+    } 
+    public string FirstName { get; } 
+    public string LastName { get; } 
+    public string FullName => $"{FirstName} {LastName}"; //整挺帅
+}
 }
